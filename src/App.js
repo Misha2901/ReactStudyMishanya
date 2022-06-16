2aa04c87463511a5f873344eade26e8f588a166e
rudenkomihajlo9 <rudenkomihajlo9@gmail.com>
2022-06-16 14:40:15 +0300

    changed for user-cards

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import UserData from './components/UserData'
-import LoadingPersonsData from './components/OnLoadingUserData'
+import Users from './components/user/user';
 
 function App() {
 
-  const DataLoading = LoadingPersonsData(UserData);
-
-  const [appState, SetAppState] = useState(
-    {
-      loading: false,
-      persons: null,
-    }
-  );
-
-
-  useEffect(() => {
-    SetAppState({ loading: true })
-    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-    axios.get(apiUrl).then((resp) => {
-      const allPersons = resp.data;
-      SetAppState({
-        loading: false,
-        persons: allPersons
-      });
-    })
-  }, [SetAppState])
-
   return (
     <div className="App">
-      <DataLoading isLoading={appState.loading} persons={appState.persons}  />
-
+      <Users />
     </div >
   );
 }
